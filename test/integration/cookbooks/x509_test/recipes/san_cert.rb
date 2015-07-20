1120b58cc85088e7d04b4832c66930de98209c7b
Peter Bell <peter_bell@peterb-mbp.local>
2015-07-20 15:16:42 -0700

    disable provision_wait in solo mode for test recipe

--- test/integration/cookbooks/x509_test/recipes/san_cert.rb
+++ test/integration/cookbooks/x509_test/recipes/san_cert.rb
@@ -6,6 +6,6 @@ x509_certificate 'service-www.example.com' do
   ca 'cshtc'
   bits 4096
   days 732
-  provisionwait true
+  provisionwait true unless Chef::Config[:solo]
   subject_alt_name [ 'www.example.com', 'example.com', 'localhost' ]
 end
